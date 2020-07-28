64e223a2ee94450abe57d508a99cd620fa45e3a3
kiran <contact@akiranraj.com>
2020-07-28 18:01:44 +1000

    Add HomeController to get all Products and setup DockerFile

---
+++ server/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using EShop.Models;
+
+namespace server.Controllers
+{
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        private static readonly List<Product> Products = new List<Product>
+        {
+            new Product 
+            {
+                Id = 1,
+                Name = "P1",
+                Price = 100.0M
+            }
+        };
+
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("api/products")]
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return Products;
+        }
+    }
+}
